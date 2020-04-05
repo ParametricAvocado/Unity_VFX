66b4641f5f31de23f409c6297b65323077807044
Mauro Delgaudio <mauro_delgaudio@outlook.com>
2020-04-04 22:53:40 -0300

    Added URP project

---
+++ UWP Project/Assets/City/Script/Turnaround.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Turnaround : MonoBehaviour
+{
+	[SerializeField] private float m_TurnaroundSpeed = 60f;
+
+	private void LateUpdate() {
+		transform.Rotate(Vector3.up, m_TurnaroundSpeed * Time.deltaTime, Space.Self);
+	}
+}
