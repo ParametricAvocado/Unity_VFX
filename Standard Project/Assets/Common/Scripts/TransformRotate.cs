de3da5a3daf0cd606f05541840ee6e45abf8f8d1
Mauro Delgaudio <mauro_delgaudio@outlook.com>
2020-08-29 14:12:16 -0300

    Orbiting particles sample

---
+++ Standard Project/Assets/Common/Scripts/TransformRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformRotate : MonoBehaviour
+{
+	public Vector3 axis;
+	public float rotation;
+	
+    void Update()
+    {
+		transform.Rotate(axis, rotation * Time.deltaTime);
+    }
+}
